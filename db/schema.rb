289cf3e431c5e8b8522c8f696ad3814457963983
Natasha Gill <natashamgill@hotmail.co.uk>
2022-08-29 15:09:55 +0100

    adding title column to book model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_29_135334) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_29_140904) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,6 +22,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_29_135334) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "title"
     t.index ["user_id"], name: "index_books_on_user_id"
   end
 
