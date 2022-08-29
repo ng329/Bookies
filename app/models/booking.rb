43a62fd95da369f9fb8ce3a5b86f5de6889a0942
Natasha Gill <natashamgill@hotmail.co.uk>
2022-08-29 20:25:36 +0100

    add linking lines and validations

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,11 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :book
+
+  validates :rental_start, :rental_end, :total_price, presence: true
+  validates :total_price, numericality: { greater_than_or_equal_to: 0,
+                                          less_than: BigDecimal(10**3) },
+                          format: {
+                            with: /\A\d(\.\d{1,2})+\z/
+                          }
 end
