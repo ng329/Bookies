c2e184304958b0ea8c573aa6f48485d4cec5e3f1
Natasha Gill <natashamgill@hotmail.co.uk>
2022-08-30 12:25:02 +0100

    creating 20 blank pages for user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+require "open-uri"
+
+puts "creating 20 books"
+
+20.times do
+  file = URI.open("https://source.unsplash.com/random/?book,cover")
+
+  title = Faker::Book.title
+  blurb = Faker::Books::Dune.quote
+  author = Faker::Book.author
+  genre = Faker::Book.genre
+  address = Faker::Address.full_address
+  price = Faker::Number.between(from: 0.0, to: 1.0).round(2)
+  user = User.all[0]
+
+  book = Book.new(
+    title:,
+    blurb:,
+    author:,
+    genre:,
+    address:,
+    price_per_day: price
+  )
+
+  book.photo.attach(io: file, filename: "book-cover.jpg", content_type: "image/jpeg")
+
+  book.user = user
+  book.save
+end
+puts "finished"
